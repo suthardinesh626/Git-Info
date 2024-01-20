74127c53218733bc5ecf06ed067f344c76d60284
Dinesh Suthar <suthardinesh626@gmail.com>
2024-01-20 18:29:57 +0530

    made reposeive for phone

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     <p><a href="${
                       user.html_url
                     }" target="_blank">GitHub Profile</a></p>
-                    <p  >${user.name || username}</p>
+                    <p>  ${user.name || username}</p>
                     <p><svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-repo UnderlineNav-octicon">
                     <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
                 </svg>  Repositories: ${user.public_repos}</p>
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .then((repos) => {
           const repoContainer = document.getElementById("repoContainer");
-          repoContainer.innerHTML = ""; // Clear previous content
+          repoContainer.innerHTML = "";
           repos.forEach((repo) => {
             const repoDiv = document.createElement("div");
             repoDiv.classList.add("repo");
@@ -72,7 +72,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
             const repoDescriptionP = document.createElement("p");
             repoDescriptionP.classList.add("repo-description");
-            repoDescriptionP.textContent = repo.description || "No description";
+            const truncatedDescription =
+              repo.description && repo.description.length > 200
+                ? repo.description.substring(0, 100) + "...(more)"
+                : repo.description || "No description";
+            repoDescriptionP.textContent = truncatedDescription;
             repoDetailsDiv.appendChild(repoDescriptionP);
 
             const repoStarsP = document.createElement("p");
